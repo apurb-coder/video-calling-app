f421770011108e939eb8f0cea634e6bf97fa8118
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-07-13 14:33:13 +0530

    chore: Add initial project files and components

---
+++ client/src/components/Top.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Top = () => {
+  return (
+    <div>
+        <div>Logo</div>
+        <div>
+            <h2>Meet-Name</h2>
+            <p>Data | Time</p>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Top
