71e4ddba2ea65478e58d8052a57c27106bd20c27
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-07-13 16:41:25 +0530

    .

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import user_add from "../assets/user-add.svg" 
-import send_logo from "../assets/Group 237540.svg"
+import React from "react";
+import user_add from "../assets/user-add.svg";
+import send_logo from "../assets/Group 237540.svg";
 
 const Chat = () => {
   return (
@@ -16,16 +16,20 @@ const Chat = () => {
         <h2 className="p-4 font-medium items-center bg-white">Chats</h2>
         <div className="p-4 font-medium items-center w-[26.3125rem] h-[16rem] bg-[#F6F6F6] rounded-md"></div>
         <div className="p-4 items-center bg-white">
-          <div className="flex space-x-24 bg-[#F6F6F6] px-5 rounded-full py-3 items-center absolute">
-            <p className="text-[#8D8F98] font-semibold text-sm">
+          <div className="relative flex w-[24rem] bg-[#F6F6F6] px-5 rounded-full py-3 items-center">
+            <p className="text-[#8D8F98] font-semibold text-sm flex-grow">
               Type something...
             </p>
-            <img src={send_logo} alt="send_logo" className="w-8 right-0" />
+            <img
+              src={send_logo}
+              alt="send_logo"
+              className="w-8 absolute right-5"
+            />
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Chat
+export default Chat;
