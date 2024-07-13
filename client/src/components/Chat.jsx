6b105d2307d93e1edba38ddfad3b64e1a86bbb7f
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-07-13 16:36:55 +0530

    .

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -14,12 +14,14 @@ const Chat = () => {
       </div>
       <div>
         <h2 className="p-4 font-medium items-center bg-white">Chats</h2>
-        {/* <div className="bg-[#F6F6F6] max-w-[26.3125rem] max-h-[52rem]"></div> */}
-        <div className="flex space-x-3 bg-[#F6F6F6] px-5 rounded-full py-3 items-center">
-          <p className="text-[#8D8F98] font-semibold text-sm">
-            Type something...
-          </p>
-          <img src={send_logo} alt="send_logo" className="w-8" />
+        <div className="p-4 font-medium items-center w-[26.3125rem] h-[16rem] bg-[#F6F6F6] rounded-md"></div>
+        <div className="p-4 items-center bg-white">
+          <div className="flex space-x-24 bg-[#F6F6F6] px-5 rounded-full py-3 items-center absolute">
+            <p className="text-[#8D8F98] font-semibold text-sm">
+              Type something...
+            </p>
+            <img src={send_logo} alt="send_logo" className="w-8 right-0" />
+          </div>
         </div>
       </div>
     </div>
