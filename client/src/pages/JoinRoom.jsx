fef2b9bfd4a969fd965d4f144805d98fb43b7b64
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-08-19 01:24:39 +0530

    .

--- client/src/pages/JoinRoom.jsx
+++ client/src/pages/JoinRoom.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Logo from "../assets/Mask group.svg";
-import { Routes, Route } from "react-router-dom";
+
 
 const JoinRoom = () => {
+  const [joinOption, setJoinOption] = useState("");
   return (
     <div className="min-h-screen flex justify-center items-center">
       <div className="flex-col space-y-8">
@@ -12,14 +13,25 @@ const JoinRoom = () => {
           <h3 className=" font-semibold text-2xl">ConnectDots</h3>
         </div>
         {/* TODO: implement conditional rendering */}
-        {/* Two Buttons */}
-        <div className='space-x-6'>
-          <button className=" py-2 px-5 bg-[#0060FF] rounded-md text-white">
-            Create New Room
-          </button>
-          <button className=" py-2 px-5 bg-[#0060FF] rounded-md text-white">
-            Join Existing room
-          </button>
+        <div>
+          {joinOption === "" && (
+            <div className="space-x-6">
+              <button
+                className=" py-2 px-5 bg-[#0060FF] rounded-md text-white"
+                onClick={() => setJoinOption("Create New Room")}
+              >
+                Create New Room
+              </button>
+              <button
+                className=" py-2 px-5 bg-[#0060FF] rounded-md text-white"
+                onClick={() => setJoinOption("Join Existing room")}
+              >
+                Join Existing room
+              </button>
+            </div>
+          )}
+          {joinOption === "Create New Room" && <div>Create New Room</div>}
+          {joinOption === "Join Existing room" && <div>Join Existing Room</div>}
         </div>
       </div>
     </div>
