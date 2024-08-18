555c9fa70381bb63fa978a6f0eb631f27e31d499
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-08-19 01:56:54 +0530

    implementetd JoinRoom.jsx

--- client/src/pages/JoinRoom.jsx
+++ client/src/pages/JoinRoom.jsx
@@ -1,9 +1,18 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 import Logo from "../assets/Mask group.svg";
-
+import { v6 as uuidv6 } from "uuid"; // to generate a unique roomID
 
 const JoinRoom = () => {
   const [joinOption, setJoinOption] = useState("");
+  const [roomID, setRoomID] = useState("");
+  const [username, setUsername] = useState("");
+  const [topic, setTopic] = useState("");
+
+  const generateRandomRoomID = () =>{
+    const randomRoomID = uuidv6();
+    setRoomID(randomRoomID);
+    console.log(username);
+  }
   return (
     <div className="min-h-screen flex justify-center items-center">
       <div className="flex-col space-y-8">
@@ -30,12 +39,67 @@ const JoinRoom = () => {
               </button>
             </div>
           )}
-          {joinOption === "Create New Room" && <div>Create New Room</div>}
-          {joinOption === "Join Existing room" && <div>Join Existing Room</div>}
+          {joinOption === "Create New Room" && (
+            <div className="flex flex-col space-y-5">
+              <input
+                type="text"
+                name="username"
+                placeholder="Enter User Name"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <input
+                type="text"
+                name="roomID"
+                id="roomID"
+                placeholder="Enter Room ID"
+                value={roomID}
+                onChange={(e) => setRoomID(e.target.value)}
+              />
+              <input
+                type="text"
+                name="topic"
+                placeholder="Enter Meeting Topic"
+                value={topic}
+                onChange={(e) => setTopic(e.target.value)}
+              />
+              <button
+                className=" py-2 px-5 bg-[#0060FF] rounded-md text-white"
+                onClick={() => generateRandomRoomID()}
+              >
+                Generate Random Room ID
+              </button>
+              <button className=" py-2 px-5 bg-[#0060FF] rounded-md text-white">
+                Join Room
+              </button>
+            </div>
+          )}
+          {joinOption === "Join Existing room" && (
+            <div className="flex flex-col space-y-5">
+              <input
+                type="text"
+                name="username"
+                placeholder="Enter User Name"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <input
+                type="text"
+                name="roomID"
+                id="roomID"
+                placeholder="Enter Room ID"
+                value={roomID}
+                onChange={(e) => setRoomID(e.target.value)}
+              />
+              <button className=" py-2 px-5 bg-[#0060FF] rounded-md text-white">
+                Join Room
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default JoinRoom
+export default JoinRoom;
