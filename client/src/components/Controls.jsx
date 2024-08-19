1633989a1a8143627e679e31f0106831ef872cfd
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-08-19 15:15:33 +0530

    Implemented Video Controls.jsx

--- client/src/components/Controls.jsx
+++ client/src/components/Controls.jsx
@@ -10,7 +10,7 @@ const Controls = () => {
       <div className="flex relative w-full items-center justify-center">
         <div className="flex space-x-5">
           {/* Toggle on/off microPhone */}
-          <div className="bg-[#0060FF] p-2 rounded-full">
+          <div className="bg-[#0060FF] p-2 rounded-full cursor-pointer">
             <img
               src={microPhoneIcon}
               alt="microPhoneIcon"
@@ -18,20 +18,20 @@ const Controls = () => {
             />
           </div>
           {/* Toggle on/off video */}
-          <div className="bg-[#0060FF] p-2 rounded-full">
+          <div className="bg-[#0060FF] p-2 rounded-full cursor-pointer">
             <img src={videoIcon} alt="videoIcon" className="w-5 h-5" />
           </div>
           {/* Share Screen */}
-          <div className="bg-[#DFEBFF] p-2 rounded-full">
+          <div className="bg-[#DFEBFF] p-2 rounded-full cursor-pointer">
             <img src={screenShareIcon} alt="videoIcon" className="w-5 h-5" />
           </div>
           {/* Record Meet */}
-          <div className="bg-[#f1f1f1] p-2 rounded-full">
+          <div className="bg-[#f1f1f1] p-2 rounded-full cursor-pointer">
             <img src={recordMeetIcon} alt="videoIcon" className="w-5 h-5" />
           </div>
         </div>
         {/* End Call */}
-        <div className=" absolute right-7">
+        <div className=" absolute right-7 cursor-pointer">
           <div className="bg-red-500 text-white py-2 px-5 rounded-full">
             End Call
           </div>
