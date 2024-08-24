34abf8305444f9107a6918cf32ea206278d9978a
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-08-24 18:50:47 +0530

    WORKING FINE!!!!!

--- server/index.js
+++ server/index.js
@@ -150,7 +150,6 @@ socket.on("leaveRoom", ({ username, room_id }) => {
       username: username,
       roomId: room_id,
       peerID: peerID,
-      room_topic: Rooms[room_id].Room_topic,
     });
   });
 
