94069598854a46337ab319d884a468974779b7b8
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-08-27 03:39:53 +0530

    Room Doesn't exist error

--- client/src/pages/Error404Room.jsx
+++ client/src/pages/Error404Room.jsx
@@ -5,8 +5,11 @@ const Error404Room = () => {
   return (
     <>
       <Top roomExits={false} topicExits={false} />
-      <div className="h-screen w-full flex justify-center items-center font-bold">
-        Such Room Doesn't Exists
+      <div className="h-screen w-full flex flex-col justify-center items-center">
+        <div className=" font-extrabold text-[#0060FF] text-2xl">404</div>
+        <div className="font-semibold text-gray-400">
+          Such Room Doesn't Exists
+        </div>
       </div>
     </>
   );
