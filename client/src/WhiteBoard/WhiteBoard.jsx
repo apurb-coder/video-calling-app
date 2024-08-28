3f8b3fdd85e3a90452cf3e48da9bf1b2b2a553b3
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-08-28 13:57:44 +0530

    WhiteBoard.jsx implemented

--- client/src/WhiteBoard/WhiteBoard.jsx
+++ client/src/WhiteBoard/WhiteBoard.jsx
@@ -1,11 +1,42 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 import { Tldraw } from "tldraw";
-import "./WhiteBoard.css"
+import "./WhiteBoard.css";
+import Error404Room from "../pages/Error404Room.jsx";
+
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 const WhiteBoard = () => {
+  const [roomExists, setRoomExists] = useState(false);
+  const { roomId } = useParams();
+
+  useEffect(() => {
+    const validateRoomID = async () => {
+      try {
+        const response = await axios.get(`${BACKEND_URL}/topic?room=${roomId}`);
+        if (response.status === 200) {
+          setRoomExists(true);
+        }
+      } catch (error) {
+        if (error.response && error.response.status === 404) {
+          setRoomExists(false);
+        }
+      }
+    };
+    validateRoomID();
+  }, [roomId]);
   return (
-    <div style={{ position: "fixed", inset: 0 }} className="whiteboard">
-      <Tldraw />
-    </div>
+    <>
+      {roomExists ? (
+        <div style={{ position: "fixed", inset: 0 }} className="whiteboard">
+          <Tldraw />
+        </div>
+      ) : (
+        <Error404Room />
+      )}
+    </>
   );
 };
 
