9def233066d2ae83446a88f96fc6399c50f5adc5
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-08-28 13:40:56 +0530

    fixed Top.jsx

--- client/src/components/Top.jsx
+++ client/src/components/Top.jsx
@@ -25,15 +25,15 @@ const Top = ({roomExits, topicExits}) => {
 
   return (
     <div className="flex justify-around">
-      <div className="p-2 flex justify-center items-center">
+      <div className="flex justify-center items-center">
         <img src={Logo} alt="logo" className="w-8" />
       </div>
-      <div className="p-2">
+      <div className="flex flex-col justify-center items-center">
         <h2 className=" text-lg font-semibold">{topicExits? topic: "Topic Invalid"}</h2>
         <p className="text-xs text-gray-400 font-semibold">{currentDateTime}</p>
       </div>
       <div
-        className="p-2 bg-[#DFEBFF] text-[#0060FF] m-4 px-8 rounded-full text-xs flex justify-center items-center space-x-1 hover:cursor-pointer"
+        className=" bg-[#DFEBFF] text-[#0060FF] m-4 px-8 py-1 rounded-full text-xs flex justify-center items-center space-x-1 hover:cursor-pointer"
         onClick={(e) => {
           navigator.clipboard.writeText(roomID);
         }}
