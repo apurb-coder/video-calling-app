1f41a96f0202a3e43005b82ebb1aadbf73b579ba
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-08-29 18:35:17 +0530

    Implemented Screen Recording functions

--- client/src/components/Controls.jsx
+++ client/src/components/Controls.jsx
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import microPhoneIcon from "../assets/microphone-2.svg";
 import videoIcon from "../assets/video.svg";
 import screenShareIcon from "../assets/send-square.svg";
 import recordMeetIcon from "../assets/Group 33.svg";
 
 const Controls = () => {
+  // For Screen Recording
+  const [isRecording, setIsRecording] = useState(false);
+  const [recordedChunks, setRecordedChunks] = useState([]);
+  const mediaRecorderRef = useRef(null);
+
+  // Start Screen Recording
+  const startRecording = async () => {
+    const stream = await navigator.mediaDevices.getDisplayMedia({
+      video: true,
+      audio: true,
+    });
+    mediaRecorderRef.current = new MediaRecorder(stream);
+    mediaRecorderRef.current.ondataavailable = (event) => {
+      if (event.data.size > 0) {
+        setRecordedChunks((prev) => [...prev, event.data]);
+      }
+    };
+    mediaRecorderRef.current.start();
+    setIsRecording(true);
+  };
+  // Stop Screen Recording
+  const stopRecording = () => {
+    mediaRecorderRef.current.stop();
+    setIsRecording(false);
+    const blob = new Blob(recordedChunks, { type: "video/webm" });
+    const url = URL.createObjectURL(blob);
+
+    // create a download-able link to download
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "meeting-recording.webm";
+    a.click();
+
+    //clean up recorded chunks
+    setRecordedChunks([]);
+  };
   return (
     <div className="flex flex-col h-[15%] items-center justify-center fixed bottom-0 left-0 right-[26.3125rem]">
       <div className="flex relative w-full items-center justify-center">
